8913c5fa5c406d74d9d29cb15e13ac21f6512a81
Harsh Shukla <harshshukla5810@gmail.com>
2025-10-24 01:35:19 +0530

    done with accent_color/colorpicker component

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -15,6 +15,7 @@ import {
 import PersonalInfoForm from '../components/PersonalInfoForm'
 import ResumePreview from '../components/ResumePreview'
 import TemplateSelector from '../components/TemplateSelector'
+import ColorPicker from '../components/ColorPicker'
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams()
@@ -119,6 +120,7 @@ const ResumeBuilder = () => {
                       setResumeData(prev => ({ ...prev, template }))
                     }
                   />
+                  <ColorPicker selectedColor={resumeData.accent_color} onChange={(color)=>setResumeData(prev=>({...prev,accent_color:color}))} />
                 </div>
                 <div className="flex items-center">
                   {activeSectionIndex !== 0 && (
