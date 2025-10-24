cf1d030f2497fca618af0142b0fac8209f22c685
Harsh Shukla <harshshukla5810@gmail.com>
2025-10-24 19:58:30 +0530

    done with projects section

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -19,6 +19,7 @@ import ColorPicker from '../components/ColorPicker'
 import ProfessionalSummaryForm from '../components/ProfessionalSummaryForm'
 import ExperienceForm from '../components/ExperienceForm'
 import EducationForm from '../components/EducationForm'
+import ProjectForm from '../components/ProjectForm'
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams()
@@ -205,6 +206,19 @@ const ResumeBuilder = () => {
                     }
                   />
                 )}
+                
+                {activeSection.id === 'projects' && (
+                  <ProjectForm
+                    data={resumeData.project}
+                    onChange={data =>
+                      setResumeData(prev => ({
+                        ...prev,
+                        project: data,
+                      }))
+                    }
+                  />
+                )}
+
               </div>
             </div>
           </div>
